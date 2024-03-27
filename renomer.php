0d26f433a6846352c658097bba19790059e732c3
Hermez Dalil <hermez.dalil@gmail.com>
2024-03-27 13:08:20 +0100

    je supprime le code mis en commentaire

--- renomer.php
+++ renomer.php
@@ -2,16 +2,7 @@
 define('BASE', 'upload/');
  
 function renommerFichier($repertoire, $nomFichier) {
-	/*$nouvNom = preg_replace('[éèë]', 'e', $nomFichier);
-	$nouvNom = preg_replace('[àä]', 'a', $nouvNom);
-	$nouvNom = preg_replace('[ùü]', 'u', $nouvNom);*/
 
-    /*
-	if (preg_match('/*.{5,}/', $nomFichier)) {$nouvNom = str_replace('.....', '.', $nomFichier); echo "cinq points (".$nouvNom.")</br>";}
-	elseif (preg_match('/*.{4,}/', $nomFichier))  {$nouvNom = str_replace('....', '.', $nomFichier); echo "quatre points(".$nouvNom.")</br>";}
-	elseif (preg_match('/*.{3,}/', $nomFichier))   {$nouvNom = str_replace('...', '.', $nomFichier); echo "trois points(".$nouvNom.")</br>";}
-	elseif (preg_match('/*.{2,}/', $nomFichier))    {$nouvNom = str_replace('..', '.', $nomFichier); echo "deux points(".$nouvNom.")</br>";}
-    */
 
     $nouvNom = str_replace('…', '', $nomFichier);
     //$nouvNom = str_replace('....', '.', $nomFichier);
@@ -61,36 +52,7 @@ function parcourirArborescence($repertoire) {
         @ closedir($dh);
     }
 } 
-/*
-function renommerFichier($repertoire, $nomFichier) {
-	$nouvNom = ereg_replace('[éèë]', 'e', $nomFichier);
-	$nouvNom = ereg_replace('[àä]', 'a', $nouvNom);
-	$nouvNom = ereg_replace('[ùü]', 'u', $nouvNom);
-	rename($repertoire . $nomFichier, $repertoire . $nouvNom);
-}
 
-function parcourirArborescence($repertoire) {
-    if (!ereg('/$', $repertoire)) {
-        $repertoire .= '/';
-    }
-    if (@ $dh = opendir($repertoire)) {
-        while (($fichier = readdir($dh)) != FALSE) {
-            if ($fichier == '.') {
-                continue; // Skip it
-            }
-            if ($fichier == '..') {
-                continue; // Skip it
-            }
-            if (is_dir($repertoire . $fichier)) {
-                parcourirArborescence($repertoire . $fichier); // Récursivité
-            } elseif (ereg('[éèëàäùü]', $fichier)) {
-				renommerFichier($repertoire, $fichier);
-            }
-        }
-        @ closedir($dh);
-    }
-}
-*/
  
 parcourirArborescence(BASE);
 ?>
